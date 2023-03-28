8b858529f1b15171d6f7bfe65352fdb5035eca6e
Ian Scott Martin <ianscottmartin@gmail.com>
2023-03-28 11:09:50 -0700

    finit

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Code your solution here
+
+const drivers = [
+    {
+        name: 'Bobby',
+        hometown: 'Pittsburgh'
+    },
+    {
+        name: 'Sammy',
+        hometown: 'New York'
+    },
+    {
+        name: 'Sally',
+        hometown: 'Cleveland'
+    },
+    {
+        name: 'Annette',
+        hometown: 'Los Angeles'
+    },
+    {
+        name: 'Bobby',
+        hometown: 'Tampa Bay'
+    }
+];
+
+
+
+
+
+function findMatching(drivers, string) {
+    return drivers.filter((drivers) => drivers.toLowerCase() === string.toLowerCase()
+    );
+}
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter(drivers => drivers.startsWith(string));
+
+}
+
+
+
+function matchName(drivers, string) {
+    return drivers.filter(drivers => drivers.name === string)
+
+
+}
